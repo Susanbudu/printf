c30fa9e9061818e6b962dc29b80485d3f9b8ab21
Susanbudu <budususan2@gmail.com>
2023-11-14 23:39:58 -0800

    printf

--- _real_printf.c
+++ _real_printf.c
@@ -1,17 +1,19 @@
 #include <stdarg.h>
-#include <unistd.h> // Assuming print_buffer uses write
+#include <unistd.h> /* Assuming print_buffer uses write*/
 
 #define BUFF_SIZE 1024
 
 void print_buffer(char buffer[], int *buffer_indicator);
 
 int _printf(const char *format, ...)
+va_list arguments;
+
 {
     if (format == NULL)
         return -1;
 
-    va_list arguments;
-    va_start(arguments, format);
+   
+	   va _start(arguments, format);
 
     int count, output = 0, buffer_indicator = 0;
     char buffer[BUFF_SIZE];
@@ -28,18 +30,18 @@ int _printf(const char *format, ...)
         else
         {
             print_buffer(buffer, &buffer_indicator);
-            // Assuming get_flags, get_width, get_precision, get_size are correctly implemented
+            /* Assuming get_flags, get_width, get_precision, get_size are correctly implemented*/
             int t = get_flags(format, &count);
             int f = get_width(format, &count, arguments);
             int d = get_precision(format, &count, arguments);
             int m = get_size(format, &count);
 
-            // Assuming _print_formatted is a function to print the formatted argument
+            /* Assuming _print_formatted is a function to print the formatted argument*/
             output += _print_formatted(t, f, d, m, arguments);
         }
     }
 
-    print_buffer(buffer, &buffer_indicator); // Print any remaining characters in the buffer
+    print_buffer(buffer, &buffer_indicator); /* Print any remaining characters in the buffer*/
     va_end(arguments);
 
     return output;
