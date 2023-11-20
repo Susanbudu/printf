8a129d733c5efe0ef61cc685fab700fd1f521883
Susanbudu <budususan2@gmail.com>
2023-11-20 05:06:02 -0800

    printf

--- _real_printf.c
+++ _real_printf.c
@@ -1,19 +1,17 @@
 #include <stdarg.h>
-#include <unistd.h> /* Assuming print_buffer uses write*/
+#include <unistd.h> /* Assuming print_buffer uses write */
 
 #define BUFF_SIZE 1024
 
 void print_buffer(char buffer[], int *buffer_indicator);
 
 int _printf(const char *format, ...)
-va_list arguments;
-
 {
     if (format == NULL)
         return -1;
 
-   
-	   va _start(arguments, format);
+    va_list arguments;
+    va_start(arguments, format);
 
     int count, output = 0, buffer_indicator = 0;
     char buffer[BUFF_SIZE];
@@ -31,13 +29,9 @@ va_list arguments;
         {
             print_buffer(buffer, &buffer_indicator);
             /* Assuming get_flags, get_width, get_precision, get_size are correctly implemented*/
-            int t = get_flags(format, &count);
-            int f = get_width(format, &count, arguments);
-            int d = get_precision(format, &count, arguments);
-            int m = get_size(format, &count);
-
+            int t = get_flags(format, &count);  
             /* Assuming _print_formatted is a function to print the formatted argument*/
-            output += _print_formatted(t, f, d, m, arguments);
+            output += _print_formatted(t, arguments);
         }
     }
 
